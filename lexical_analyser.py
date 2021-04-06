649195443677743ed05d23d86c4285a601943463
Omprakash <59120734+opmindia13@users.noreply.github.com>
2021-04-06 13:20:45 +0530

    Add files via upload

---
+++ lexical_analyser.py
@@ -0,0 +1,74 @@
+#List of Keywords
+keywords = ["if", "else", "while", "do", "break",
+			"continue", "int", "double", "float", 
+			"return", "char", "sizeof", "typedef",
+			"switch", "void", "static", "struct", "main"]
+			
+
+#List of Operators
+operators = ['+', '-', '*', '/',
+			 '>', '<', '=']
+
+#List of Seprators
+seprators = ['+', '-', '*',
+			 '/', ',', ';', '>',
+			 '<', '=', '(', ')',
+			 '[', ']', '{', '}']
+
+# Function to classify the tokens
+def identify(st:str):
+	if st.isdecimal():
+		return "Integer"
+	elif st in keywords:
+		return "Keyword"
+	elif st in operators:
+		return "Operator"
+	elif st in seprators and st not in operators:
+		return "Seprator"
+	else:
+		return "User defined Identifier"
+
+#------------------------------------------------------------
+
+count = 0 #token count
+line = input(">>>> ")
+tokens = line.split(' ')
+#print(tokens)
+print("\n")
+
+for t in tokens:
+	
+	if len(t) != 1:
+		temp = ""
+		#print("t = ",t)
+		for i in t:
+			#print("i = ", i)
+			if i in seprators:
+				if len(temp) != 0:
+					result = identify(temp)
+					print(temp, ": ",result)
+					count+=1
+
+				result = identify(i)
+				print(i, ": ",result)
+				count+=1
+				#print(temp)
+				temp=""
+			else :
+				temp += i
+				#print(temp)
+
+		if len(temp) != 0:
+			result = identify(temp)
+			print(temp, ": ",result)
+			count+=1
+
+	else:
+		result = identify(t)
+		print(t, ": ",result)
+		count+=1
+
+print("\nTotal token identified: ",count)
+
+
+
